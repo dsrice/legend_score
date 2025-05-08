dd874a6f0a73199f15003c6dec1427d97ee905ca
dsrice <v.fuji.0223@gmail.com>
2025-05-09 00:43:26 +0900

    feat: Github Action対応

--- be/infra/database/connection/conn_test.go
+++ be/infra/database/connection/conn_test.go
@@ -2,7 +2,6 @@ package connection_test
 
 import (
 	"legend_score/infra/database/connection"
-	"os"
 	"testing"
 
 	"github.com/stretchr/testify/suite"
@@ -33,29 +32,5 @@ func (s *ConnectionSuite) TestGetConnection() {
 }
 
 func TestConnectionSuite(t *testing.T) {
-	// Set GO_ENV to "test" to use test.env
-	os.Setenv("GO_ENV", "test")
-
-	// Print the current working directory to help debug
-	dir, err := os.Getwd()
-	if err != nil {
-		t.Logf("Error getting current directory: %v", err)
-	} else {
-		t.Logf("Current directory: %s", dir)
-	}
-
-	// Check if test.env exists in various locations
-	_, err = os.Stat("test.env")
-	t.Logf("test.env in current dir exists: %v", err == nil)
-
-	_, err = os.Stat("../test.env")
-	t.Logf("../test.env exists: %v", err == nil)
-
-	_, err = os.Stat("../../test.env")
-	t.Logf("../../test.env exists: %v", err == nil)
-
-	_, err = os.Stat("../../../test.env")
-	t.Logf("../../../test.env exists: %v", err == nil)
-
 	suite.Run(t, new(ConnectionSuite))
 }
