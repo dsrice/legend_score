16f271e92343bb52ee793c5e8053eef3c4eb9ab8
dsrice <v.fuji.0223@gmail.com>
2025-05-09 00:32:49 +0900

    feat: テストコードを実行できるように宣言見直し

--- be/infra/database/connection/conn.go
+++ be/infra/database/connection/conn.go
@@ -36,8 +36,90 @@ func getConnection() (*sql.DB, error) {
 		return nil, err
 	}
 
-	err = godotenv.Load(fmt.Sprintf("%s/%s.env", os.Getenv("ROOT_PATH"), os.Getenv("GO_ENV")))
-	if err != nil {
+	ge := os.Getenv("GO_ENV")
+	if ge == "" {
+		ge = "dev"
+	}
+
+	// Try to load the environment file from various locations
+	// First, try the current directory
+	envLoaded := false
+
+	// Try test.env first if in test mode
+	if ge == "test" {
+		err = godotenv.Load("test.env")
+		if err == nil {
+			envLoaded = true
+		}
+	}
+
+	// If test.env wasn't loaded or GO_ENV is not "test", try {GO_ENV}.env
+	if !envLoaded {
+		envFile := fmt.Sprintf("%s.env", ge)
+		err = godotenv.Load(envFile)
+		if err == nil {
+			envLoaded = true
+		}
+	}
+
+	// If still not loaded, try looking in parent directories
+	if !envLoaded {
+		// Try one level up
+		if ge == "test" {
+			err = godotenv.Load("../test.env")
+			if err == nil {
+				envLoaded = true
+			}
+		}
+
+		if !envLoaded {
+			envFile := fmt.Sprintf("../%s.env", ge)
+			err = godotenv.Load(envFile)
+			if err == nil {
+				envLoaded = true
+			}
+		}
+
+		// Try two levels up
+		if !envLoaded {
+			if ge == "test" {
+				err = godotenv.Load("../../test.env")
+				if err == nil {
+					envLoaded = true
+				}
+			}
+
+			if !envLoaded {
+				envFile := fmt.Sprintf("../../%s.env", ge)
+				err = godotenv.Load(envFile)
+				if err == nil {
+					envLoaded = true
+				}
+			}
+		}
+
+		// Try three levels up (be directory)
+		if !envLoaded {
+			if ge == "test" {
+				err = godotenv.Load("../../../test.env")
+				if err == nil {
+					envLoaded = true
+				}
+			}
+
+			if !envLoaded {
+				envFile := fmt.Sprintf("../../../%s.env", ge)
+				err = godotenv.Load(envFile)
+				if err == nil {
+					envLoaded = true
+				}
+			}
+		}
+	}
+
+	// If we still couldn't load any environment file, return an error
+	if !envLoaded {
+		err = fmt.Errorf("could not load any environment file for GO_ENV=%s", ge)
 		logger.Error(err.Error())
 		return nil, err
 	}
