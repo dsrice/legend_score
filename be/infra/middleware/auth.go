e005e8fcf50830688cfedfec2ee586c17250b580
dsrice <v.fuji.0223@gmail.com>
2025-05-11 22:59:10 +0900

    feat: JWT認証周りがおかしいので見直し

--- be/infra/middleware/auth.go
+++ be/infra/middleware/auth.go
@@ -5,6 +5,7 @@ import (
 	"github.com/labstack/echo/v4"
 	"legend_score/consts/ecode"
 	"legend_score/controllers"
+	"legend_score/infra/logger"
 	"net/http"
 	"os"
 	"strconv"
@@ -16,13 +17,13 @@ func JWTMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		authHeader := c.Request().Header.Get("Authorization")
 		if authHeader == "" {
-			return controllers.ErrorResponse(c, ecode.E0001)
+			return controllers.ErrorResponse(c, ecode.E0000)
 		}
 
 		// Check if the Authorization header has the Bearer prefix
 		parts := strings.Split(authHeader, " ")
 		if len(parts) != 2 || parts[0] != "Bearer" {
-			return controllers.ErrorResponse(c, ecode.E0001)
+			return controllers.ErrorResponse(c, ecode.E0000)
 		}
 
 		tokenString := parts[1]
@@ -35,6 +36,7 @@ func JWTMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
 		})
 
 		if err != nil {
+			logger.Error(err.Error())
 			return controllers.ErrorResponse(c, ecode.E0000)
 		}
 
